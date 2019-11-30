d75fe9e387e37e3a036b09102161ce8d928faf41
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2019-12-01 01:43:44 +0300

    Updates

---
+++ precache-manifest.6cbd87dacc1590ac25d775a95b509297.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6235bd32ca11a5413599b8e93f17dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2d6b48bfcaa62839b8b",
+    "url": "/static/css/main.a3c5ecfb.chunk.css"
+  },
+  {
+    "revision": "79721f64925e7f264954",
+    "url": "/static/js/2.56a3f7af.chunk.js"
+  },
+  {
+    "revision": "e2d6b48bfcaa62839b8b",
+    "url": "/static/js/main.73fc171c.chunk.js"
+  },
+  {
+    "revision": "cc81e1ecd3b1c544e0a9",
+    "url": "/static/js/runtime-main.2ccca137.js"
+  },
+  {
+    "revision": "d330db22d7b75102aab523484ee41a75",
+    "url": "/static/media/company.d330db22.svg"
+  },
+  {
+    "revision": "afb6062ab739c446a46dd11c346ffd0a",
+    "url": "/static/media/email.afb6062a.svg"
+  },
+  {
+    "revision": "a4e1e7d6263431bf79a4611faa872317",
+    "url": "/static/media/location.a4e1e7d6.svg"
+  }
+]);
